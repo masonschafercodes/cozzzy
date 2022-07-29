ca425a1db54dbb9f02eaa40093610935c44a6511
Mason Schafer <mason.schafer1@gmail.com>
2022-07-28 22:13:47 -0400

    feat: teams and team invites

---
+++ prisma/migrations/20220729003849_add_teams_and_teaminvites/migration.sql
@@ -0,0 +1,32 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "teamId" TEXT;
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TeamInvite" (
+    "id" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TeamInvite_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "TeamInvite" ADD CONSTRAINT "TeamInvite_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TeamInvite" ADD CONSTRAINT "TeamInvite_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,23 @@
 import { useSession } from 'next-auth/react';
 import Navbar from '../ui/Navbar';
 
-export function Home() {
+export function Home({ user, hasTeam }: { user: any; hasTeam: boolean }) {
 	const { data: session } = useSession();
 
 	return (
 		<div>
 			<div>
 				<Navbar session={session} />
+				<div>
+					<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+						<pre>
+							<code>{JSON.stringify(user, null, 2)}</code>
+						</pre>
+						<pre>
+							<code>On a Team: {JSON.stringify(hasTeam, null, 2)}</code>
+						</pre>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
